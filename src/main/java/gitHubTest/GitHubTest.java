8a4f87e5612b4ff2235b84d6d57dca700499f4d4
cargerlop <cargerlop@hotmail.com>
2018-08-12 12:33:11 -0300

    added src folder

---
+++ src/main/java/gitHubTest/GitHubTest.java
@@ -0,0 +1,11 @@
+package gitHubTest;
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
