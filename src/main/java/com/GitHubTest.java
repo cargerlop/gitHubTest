6ca06bad8e77199ae5ff782a756d0acff120c6cd
cargerlop <cargerlop@hotmail.com>
2020-07-18 00:52:12 -0300

    new method added

--- src/main/java/com/GitHubTest.java
+++ src/main/java/com/GitHubTest.java
@@ -1,6 +1,7 @@
 package com;
 
-import java.lang.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GitHubTest {
 
@@ -8,13 +9,22 @@ public class GitHubTest {
 		System.out.println("Hello World!");
 		System.out.println("############");
 		
-		int[] numbers = new int[]{1,5,7,8,9}; 
+		int[] numbers = new int[]{1,5,7,8,9,6}; 
 		int target = 9;
-		twoNumbersFromListToGetTarget(numbers, target);
+		int[] result = twoNumbersFromListToGetTarget(numbers, target);
+		System.out.println("1: "+result[0]+" 2: "+result[1]);
 	}
 
 	public static int[] twoNumbersFromListToGetTarget(int[] numbers, int target){
-		
+		Map<Integer, Integer> processed = new HashMap<>();
+		for(int i = 0; i < numbers.length; i++){
+			Integer delta = target - numbers[i];
+			if(processed.containsKey(delta)){
+				return new int[]{processed.get(delta),i};
+			}else{
+				processed.put(numbers[i], i);
+			}
+		}
 		return new int[]{0,0};
 	}
 }
